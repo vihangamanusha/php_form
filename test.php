84c61886388cb88885c317e97de4b4081e858bee
vihangamanusha <vihangamanusha07@gmail.com>
2024-11-08 16:11:06 +0530

    read afile from text

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
    $file="myfile.txt";
-   $handle=fopen($file,"w") or die('can not open the file :'.$file);
-    
+   $handle=fopen($file,"r") or die('can not open the file :'.$file);  
+   $filesize=filesize($file);
+   $filetext=fread($handle,$filesize);
+   fclose($handle);
 
+   echo("file size $filesize and text is $filetext");
+   
 ?>

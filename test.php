16064188dd0ccd12efcae56f9722df9a58f314ef
vihangamanusha <vihangamanusha07@gmail.com>
2024-11-09 09:16:40 +0530

    updating  connection

--- test.php
+++ test.php
@@ -18,9 +18,34 @@
    if(!$db_selected){
     die ('con not use database '.mysqli_error($conn));
    }else{
-    echo ("can use database $database");
+    echo ("can use database $database"."<br>");
+   }
+   
+   $sql_table='CREATE TABLE IF NOT EXISTS student(
+                   NIC INT(2)AUTO_INCREMENT,
+                   NAME VARCHAR(50),
+                   ADDERESS VARCHAR(250),
+                   PRIMARY KEY(NIC)
+   ) ';
+
+   if(!mysqli_query($conn,$sql_table)){
+    die('can not create table'.mysqli_error($conn));
+   }else{
+    echo 'create table succesfully';
    }
 
+   $sql_data_insert="INSERT INTO student
+                        (NAME,ADDERESS)
+                     VALUES
+                        ('Vihanga','dehiowita'),
+                        ('kavindu','awissawella')";
+   if(!mysqli_query($conn,$sql_data_insert)){
+      die ('can not insrt data to table :'.mysqli_error($conn));
+   }else{
+      echo 'data insert successfully';
+   }
+   
+   
    mysqli_close($conn);
 
 
